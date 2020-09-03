954edb0b2da401a937d6dead56b4cafd9e6068d7
Manuel Alejandro <manumunizcap@gmail.com>
2020-09-03 15:31:23 -0300

    Cambios para que el textbox del login se vacie

--- ProyectoProgramacion/ProyectoProgramacion/Modulos/ValidacionesBasicas.vb
+++ ProyectoProgramacion/ProyectoProgramacion/Modulos/ValidacionesBasicas.vb
@@ -10,6 +10,7 @@ Friend Class ValidacionesBasicas
             verdaderoFalso = False
         ElseIf Not validador.IsMatch(txtValidarContraseña.Text) Then
             FormInfo.callFormWithMessage("El formato de la contraseña debe de ser solo numérico y tiene que tener un mínimo de 7 carácteres")
+            txtValidarContraseña.Text = ""
             verdaderoFalso = False
         Else
             verdaderoFalso = True
@@ -26,6 +27,7 @@ Friend Class ValidacionesBasicas
             verdaderoFalso = False
         ElseIf Not validador.IsMatch(txtValidarTelefono.Text) Then
             FormInfo.callFormWithMessage("El formato debe de ser solo numérico y tiene que tener estrictamente 9 carácteres")
+            txtValidarTelefono.Text = ""
             verdaderoFalso = False
         Else
             verdaderoFalso = True
@@ -40,7 +42,8 @@ Friend Class ValidacionesBasicas
             FormInfo.callFormWithMessage("El campo de texto no puede estar vacío")
             verdaderoFalso = False
         ElseIf Not validador.IsMatch(txtValidarTexto.Text) Then
-            FormInfo.callFormWithMessage("El campo de texto debe ser solo alfanumérico y tiene que tener un mínimo de 7 carácteres")
+            FormInfo.callFormWithMessage("El campo de texto solo debe de tener letras y tiene que tener un mínimo de 7 carácteres")
+            txtValidarTexto.Text = ""
             verdaderoFalso = False
         Else
             verdaderoFalso = True
